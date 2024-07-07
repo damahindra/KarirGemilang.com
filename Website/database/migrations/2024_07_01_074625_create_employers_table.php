bce71c4c429bb5620dc28964fee19ebe86e0cc08
damahindra <ranggaandhito@gmail.com>
2024-07-07 19:47:52 +0700

    Fix Exployers Structure

--- Website/database/migrations/2024_07_01_074625_create_employers_table.php
+++ Website/database/migrations/2024_07_01_074625_create_employers_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('password');
             $table->string('phone_number');
             $table->date('birthdate');
-            $table->string('last_education')->nullable();
+            $table->string('position')->nullable();
             $table->rememberToken();
             $table->timestamps();
 
