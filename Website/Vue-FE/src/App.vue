7859c480dc81bc49d5a8aaa3b14224633c2dd9ad
ghozt666 <sgnasurullah@gmail.com>
2024-07-13 10:46:03 +0700

    testing html

--- Website/Vue-FE/src/App.vue
+++ Website/Vue-FE/src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SplashScreen msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SplashScreen from './components/SplashScreen.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SplashScreen
   }
 }
 </script>
---
+++ Website/Vue-FE/src/components/SplashScreen.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="main-container">
+      <div class="rectangle">
+        <div class="rectangle-1"><span class="daftar">Daftar</span></div>
+        <div class="rectangle-2"><span class="masuk">Masuk</span></div>
+        <div class="group"></div>
+        <span class="karir-gemilang">Karir Gemilang</span
+        ><span class="home">Home</span><span class="about">About</span
+        ><span class="jobs">Jobs</span><span class="contact-us">Contact Us</span>
+      </div>
+      <div class="texture">
+        <div class="background"><div class="vector"></div></div>
+        <div class="objects">
+          <div class="group-3"><div class="clip-path-group"></div></div>
+        </div>
+      </div>
+      <div class="image"></div>
+      <span class="browse-apply-prepare"
+        >Browse. Apply.<br />Prepare Your Future</span
+      ><span class="text-9"
+        >Cari pekerjaan impian dengan cepat dan mudah bersama Karir
+        Gemilang!</span
+      >
+      <div class="box-4"><span class="text-a">Cari kerja sekarang!</span></div>
+    </div>
+  </template>
+  
+  <script setup lang="javascript"></script>
+  
+
+  
